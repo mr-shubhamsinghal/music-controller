cc5bd47d39a21cdc16dab15b6b71d6dd08e074e4
Shubham Singhal <singhal121997@gmail.com>
2020-12-26 12:08:14 +0530

    Handling POST Requests (Django REST)

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 
 urlpatterns = [
-    path('room/', views.RoomView.as_view())
+    path('room/', views.RoomView.as_view()),
+    path('create-room/', views.CreateRoomView.as_view())
 ]
